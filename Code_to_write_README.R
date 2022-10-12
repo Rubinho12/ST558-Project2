0731ae0cf45271ca9dc70f83f8aa4ceedbd006a3
Ruben Sowah <rubensowah@gmail.com>
2022-10-12 18:43:17 -0400

    one commit

---
+++ Code_to_write_README.R
@@ -0,0 +1,6 @@
+rmarkdown::render("ST558-Project2 rmd.Rmd",
+                  output_format = "github_document",
+                  output_file = "README.md",
+                  output_options = list(
+                    html_preview = FALSE, toc = TRUE, toc_depth = 2, toc_float = TRUE))
+
